7f5a0aeedda1394fec1fd2c47429c9a1b40d9f63
Bradin-b <babfdb@umsystem.edu>
2024-03-08 20:40:09 -0600

    BasicPagesUIs Committed here

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostTestComponent } from './post/post-test/post-test.component';
+import { InputComponent } from './input/input.component';
+import { FormComponent } from './form/form.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { LoginComponent } from './login/login.component';
+import { API1Component } from './api1/api1.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"", component:PostTestComponent},
+  {path:"input", component:InputComponent },
+  {path:"contactus", component:FormComponent},
+  {path:"price", component:PricingComponent},
+  {path:"login", component:LoginComponent},
+  {path:"api1", component:API1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,48 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { PostTestComponent } from './post/post-test/post-test.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
+import { NavbarComponent } from './post/navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { InputComponent } from './input/input.component';
+import { FormComponent } from './form/form.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormControl } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PricingComponent } from './pricing/pricing.component';
+import { LoginComponent } from './login/login.component';
+import { API1Component } from './api1/api1.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PostTestComponent
+    PostTestComponent,
+    NavbarComponent,
+    InputComponent,
+    FormComponent,
+    PricingComponent,
+    LoginComponent,
+    API1Component,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideAnimationsAsync()
